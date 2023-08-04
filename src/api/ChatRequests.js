bb7f5b6695ef802ea54323c5ca0424238f1b754e
puriravindra412 <puriravindra412@gmail.com>
2023-08-04 17:25:48 +0530

    updated commit

--- src/api/ChatRequests.js
+++ src/api/ChatRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://communexbackend.onrender.com" });
 
 export const createChat = (data) => API.post("/chat/", data);
 
--- src/api/CommunityRequests.js
+++ src/api/CommunityRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://communexbackend.onrender.com" });
 export const getCommunity = () => API.get(`/community/getCommunity`);
 export const createCommunity = (data) => API.post("/community/", data);
 export const addUserToCommunity = (data) => API.put("/community/addUser", data);
