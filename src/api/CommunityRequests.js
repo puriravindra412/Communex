2b3dd4de5ff2809528c4963418c0577e0cffa5b9
puriravindra412 <puriravindra412@gmail.com>
2023-08-22 15:09:39 +0530

    new

--- src/api/CommunityRequests.js
+++ src/api/CommunityRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://communexbackend.onrender.com/" });
 export const getCommunity = () => API.get(`/community/getCommunity`);
 export const getRecentUser = () => API.get(`/community/recentUser`)
 export const createCommunity = (data) => API.post("/community/", data);
--- src/api/SearchRequest.js
+++ src/api/SearchRequest.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://communexbackend.onrender.com/" });
 export const searchResult = (search) => API.get(`/search/${search}`);
