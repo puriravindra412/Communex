bf7f7fe4cc8ef516b54bbb537b36aa545582340b
puriravindra412 <puriravindra412@gmail.com>
2023-08-23 16:26:06 +0530

    new

--- src/components/community/GetCommunityPost.js
+++ src/components/community/GetCommunityPost.js
@@ -8,22 +8,29 @@ import "../../css/profile.css";
 import FeedCard from "../home/feedCard.js";
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import { useSelector } from "react-redux";
-import { UserCard } from "../user/UserCard.js";
-
+import { UserCard } from "../user/UserCard.js"
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { CircularProgress } from "@mui/material";
 export const GetCommunityPost = () => {
   const location = useLocation();
   const [img, peoples, posts] = location.state;
-
+  const [loading,setLoading]=useState(true)
   const params = useParams();
   const name = params.name;
   const [postDiv, setPostDiv] = useState(true);
   const [userDiv, setUserDiv] = useState(false);
   const [Community, setCommunity] = useState([]);
-
+  const [value, setValue] = React.useState('1');
   useEffect(() => {
     const communities = async () => {
+      setLoading(true);
       const communitData = await CommunityApi.getCommunityPost(name);
       setCommunity(communitData.data);
+      setLoading(false)
     };
     communities();
   }, [name]);
@@ -45,8 +52,8 @@ export const GetCommunityPost = () => {
   console.log("inside get community " ,Community)
   const [activeCategory, setActiveCategory] = useState("user");
 
-  const handleCategoryChange = (category) => {
-    setActiveCategory(category);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
   const users = peoples.filter((people) => {
     return people.userId !== user._id;
@@ -92,48 +99,48 @@ export const GetCommunityPost = () => {
             
             
             </div>
+            
             <div className="community-post-container">
-             <div className="card-container ">
-                <div className="container-top">
-                  <div className="buttons-container">
-                    <div>
-                      <button
-                        className={activeCategory === "post" ? "active" : ""}
-                        onClick={() => handleCategoryChange("post")}
-                      >
-                        Post
-                      </button>
-                      <button
-                        className={activeCategory === "user" ? "active" : ""}
-                        onClick={() => handleCategoryChange("user")}
-                      >
-                        User
-                      </button>
-                    </div>
-                    <hr></hr>
-                  </div>
-                </div>
-
-                {activeCategory === "post" &&
+            <div className="card-container">
+          
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value} >
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList onChange={handleChange} aria-label="lab API tabs example" >
+                  <Tab label="Posts" value="1" sx={{color:"var(--color)"}}/>
+                  <Tab label="Users" value="2" sx={{color:"var(--color)"}}/>
+                  
+                </TabList>
+              </Box>
+              <TabPanel  sx={{padding:0}} value="1">
+              {loading?<CircularProgress sx={{margin:"30px 45%"}} /> : <div className="category-content">
+              {
                   Community?.result?.post.map((post, id) => {
                     return <FeedCard data={post} key={id} />;
                   })}
-                {activeCategory === "user" && (
-                  <div className="community-users">
-                    {Community?.result?.usersData.filter((people)=>{return people._id !== user._id}).map((people, index) => {
-                      return (
-                        <UserCard
-                          username={people.username}
-                          profile={people.profilePicture}
-                          userId={people._id}
-                          
-                          worksAt={people.worksAt}
-                          key={index}
-                        />
-                      );
-                    })}
+                </div>}
+            </TabPanel>
+            <TabPanel  sx={{padding:0}} value="2">
+              {loading?<CircularProgress sx={{margin:"30px 45%"}} /> : <div className="category-content">
+              <div className="community-users">
+              {Community?.result?.usersData.filter((people)=>{return people._id !== user._id}).map((people, index) => {
+                return (
+                  <UserCard
+                    username={people.username}
+                    profile={people.profilePicture}
+                    userId={people._id}
+                    
+                    worksAt={people.worksAt}
+                    key={index}
+                  />
+                );
+              })}
                   </div>
-                )}
+              
+                </div>}
+            </TabPanel>
+            </TabContext>
+            </Box>
               </div>
             </div>
           </div>
